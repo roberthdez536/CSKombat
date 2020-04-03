288a3d686c018d56370a7cfbbc58f8e75733c489
roberthdez536 <57495184+roberthdez536@users.noreply.github.com>
2020-04-03 17:26:58 -0500

    big rob demo

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float SPEED = 5.0f;
+    [SerializeField]
+    private float JUMP = 10.0f;
+    [SerializeField]
+    private float GRAVITY = 25.0f;
+
+    CharacterController cc;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.isGrounded)
+        {
+            movement = transform.right * Input.GetAxis("Horizontal");
+            movement = transform.TransformDirection(movement);
+            movement *= SPEED;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                movement += Vector3.up * JUMP;
+            }
+
+        }
+        else
+        {
+            float buff = movement.y;
+            movement = transform.right * Input.GetAxis("Horizontal");
+            movement = transform.TransformDirection(movement);
+            movement *= SPEED;    
+            movement.y = buff;
+        }
+        movement += Vector3.down * GRAVITY * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        cc.Move(movement * Time.deltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CSKombat
  
+Please copy and paste my .unity scene file and rename it to yours so we all edit separate scene files
